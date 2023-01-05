318b4757f2815646051144db643eb44a6d6f7f72
mrcsporto <mrcsporto@gmail.com>
2023-01-05 15:47:34 -0300

    add first section

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,22 +2,20 @@ import "./styles.css";
 
 const Intro = () => {
   return (
-
-    <div className="d-flex flex-column align-items-center">
-      <h2 className="title fs-5 mt-5">Designed for the future</h2>
-
+    <div className="d-flex flex-column align-items-center design-section">
+      <h2 className="title fs-4 mt-5">Designed for the future</h2>
       <div className="container-fluid d-flex flex-wrap flex-row align-items-center">
-        <div className="col-sm-6">
-          <div className="card border-0">
-            <div className="card-body">
-              <h5 className="card-title">Introducing an extensible editor</h5>
+        <div className="col-sm-6 ps-5">
+          <div className="card border-0 ps-5">
+            <div className="card-body ps-5">
+              <h5 className="card-title mb-3">Introducing an extensible editor</h5>
               <p className="card-text"> Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content.
                 The editor supports management of multiple blogs and allows easy manipulation of embeds such as images,
                 videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or
                 change the looks of a blog.</p>
             </div>
-            <div className="card-body">
-              <h5 className="card-title">Robust content management</h5>
+            <div className="card-body ps-5">
+              <h5 className="card-title mb-3">Robust content management</h5>
               <p className="card-text">Flexible content management enables users to easily move through posts. Increase the usability of your blog
                 by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.</p>
             </div>
