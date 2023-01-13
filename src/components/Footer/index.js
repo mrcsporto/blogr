217cb3853be360743fd9bdda38b9e01cb729dddc
mrcsporto <mrcsporto@gmail.com>
2023-01-13 14:28:18 -0300

    fix links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,37 +7,37 @@ const Footer = () => {
         <h1 className="text-white fs-1 ms-0 ms-md-5">Blogr</h1>
         <ul className="navbar-nav d-sm-flex flex-sm-row  flex-column flex-grow-1 justify-content-evenly">
           <li className="nav-item">
-            <a className='nav-link text-white mb-3 mt-5 mt-sm-0' href="/">
+            <a className='nav-link text-white mb-3 mt-5 mt-sm-0' href="/blogr">
               Product
             </a>
             <ul className="list-group">
-              <li><a href="/">Overview</a></li>
-              <li><a href="/">Pricing</a></li>
-              <li><a href="/">Marketplace</a></li>
-              <li><a href="/">Features</a></li>
-              <li><a href="/">Integrations</a></li>
+              <li><a href="/blogr">Overview</a></li>
+              <li><a href="/blogr">Pricing</a></li>
+              <li><a href="/blogr">Marketplace</a></li>
+              <li><a href="/blogr">Features</a></li>
+              <li><a href="/blogr">Integrations</a></li>
             </ul>
           </li>
           <li className="nav-item">
-            <a className='nav-link text-white mb-3 mt-4 mt-sm-0' href="/">
+            <a className='nav-link text-white mb-3 mt-4 mt-sm-0' href="/blogr">
               Company
             </a>
             <ul className="list-group">
-              <li><a href="/">About</a></li>
-              <li><a href="/">Team</a></li>
-              <li><a href="/">Blog</a></li>
-              <li><a href="/">Careers</a></li>
+              <li><a href="/blogr">About</a></li>
+              <li><a href="/blogr">Team</a></li>
+              <li><a href="/blogr">Blog</a></li>
+              <li><a href="/blogr">Careers</a></li>
             </ul>
           </li>
 
           <li className="nav-item">
-            <a className='nav-link text-white mb-3 mt-4 mt-sm-0' href="/">
+            <a className='nav-link text-white mb-3 mt-4 mt-sm-0' href="/blogr">
               Connect
             </a>
             <ul className="list-group">
-              <li><a href="/">Contact</a></li>
-              <li><a href="/">Newsletter</a></li>
-              <li><a href="/">LinkedIn</a></li>
+              <li><a href="/blogr">Contact</a></li>
+              <li><a href="/blogr">Newsletter</a></li>
+              <li><a href="/blogr">LinkedIn</a></li>
             </ul>
           </li>
         </ul>
